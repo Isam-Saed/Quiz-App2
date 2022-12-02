fffb2a1f3608c42c91c4d20d1dcd9a1e295ab6f0
Isam Ali <119074096+Isam-Saed@users.noreply.github.com>
2022-12-02 15:03:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ let result = document.querySelector('.result');
 let countDown = document.querySelector('.countDown');
 let Student = prompt(`Welcome in Quiz App .. Please Enter Your Name.`);
 
-currentIndex=0
+let currentIndex=0;
+let countDownInterval = function(){
+    
+}
 
 function getQuestions(){
     let myRequest = new XMLHttpRequest();
@@ -20,7 +23,8 @@ function getQuestions(){
 let questionObject = JSON.parse(this.responseText);
 let QCount = questionObject.length;
 createBullets(QCount);
-console.log()
+addQuestionsData(questionObject[currentIndex],QCount);
+countDownfun(15,QCount)
        } 
        
     }
@@ -42,4 +46,53 @@ function createBullets(num){
         bulletSpanContainer.appendChild(theBullet);
         if(i === 0){theBullet.className='on';}
     }
+}
+
+// To add Question and ansewer 
+function addQuestionsData(obj,countQ){
+if(currentIndex < countQ){
+    let questionTitle = document.createElement('h2');
+    questionTitle.className='wow flipInY';
+    let questionTExt =document.createTextNode(obj["title"]);
+    questionTitle.appendChild(questionTExt);
+    quizArea.appendChild(questionTitle);
+
+    for(let i = 1 ; i<=4; i ++){
+let mainDiv = document.createElement('div');
+mainDiv.className='answer wow fadeInDown';
+let radioInput = document.createElement('input');
+radioInput.name='question';
+radioInput.type='radio';
+radioInput.id=`answer_${i}`;
+radioInput.dataset.answer = obj[`answer_${i}`];
+if(i === 1 ){radioInput.checked=true}
+
+let theLabel = document.createElement('label');
+let TheLableText =document.createTextNode(obj[`answer_${i}`])
+theLabel.htmlFor =`answer_${i}`;
+theLabel.appendChild(TheLableText);
+mainDiv.appendChild(radioInput)
+mainDiv.appendChild(theLabel);
+answerArea.appendChild(mainDiv)
+    }
+}
+}
+
+function countDownfun(duration,count4){
+if(currentIndex < count4 ){
+    let minutes,secunds;
+let countDownInterval= setInterval(function(){
+    minutes=parseInt(duration / 60);
+    secunds=parseInt(duration % 60);
+
+    minutes =minutes<10 ?`0${minutes}`: minutes;
+    secunds =secunds<10 ?`0${secunds}`: secunds;
+
+    countDown.innerHTML=`${minutes}:${secunds}`
+    if(--duration < 0){
+        clearInterval(countDownInterval);
+        submitButton.click();
+    }
+} ,1000)
+}
 }
