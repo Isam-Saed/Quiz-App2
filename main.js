370aa1ff10bbac07a8939c170a4dcae1c5654047
Isam Ali <119074096+Isam-Saed@users.noreply.github.com>
2022-12-02 18:19:18 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 
+
 let countSpan = document.querySelector(".count span");
 let bulletSpanContainer = document.querySelector('.bullets .spans')
 let quizArea = document.querySelector('.quiz-area');
@@ -9,129 +10,116 @@ let submitButton = document.querySelector('.btn');
 let result = document.querySelector('.result');
 let countDown = document.querySelector('.countDown');
 let Student = prompt(`Welcome in Quiz App .. Please Enter Your Name.`);
-
-let currentIndex=0;
-let rightAnswer =0;
-let countDownInterval = function(){
-    
-}
-
+//set Data
+let currentIndex =0;
+let rightAnswer   =0;
+let countDownInterval;
 function getQuestions(){
     let myRequest = new XMLHttpRequest();
-
-    myRequest.onreadystatechange = function(){
-       if(this.readyState === 4 && this.status === 200){
-        let questionObject = JSON.parse(this.responseText);
-        let QCount = questionObject.length;
-        createBullets(QCount);
-        addQuestionsData(questionObject[currentIndex],QCount);
-        countDownfun(15,QCount);
-
-
-        submitButton.onclick=()=>{
+    myRequest.onreadystatechange= function(){
+        if(this.readyState === 4 && this.status === 200){
+          let questionObject = JSON.parse(this.responseText)
+          let QCount = questionObject.length;
+          createBullets(QCount)
+          addQuestionsData(questionObject[currentIndex],QCount)
+          countDownfun(15,QCount);
+          
+          submitButton.onclick=()=>{
             let theRightAnswer = questionObject[currentIndex].right_answer;
             currentIndex++;
             checkAnswer(theRightAnswer,QCount);
+
             quizArea.innerHTML   ='';
             answerArea.innerHTML ='';
             addQuestionsData(questionObject[currentIndex],QCount);
+            
             handelBullet();
+            
             clearInterval(countDownInterval);
-            countDownfun(15,QCount);
+            countDownfun(15,QCount)
             showResult(QCount);
+            
+                                    };
+             
+                              
         }
-       } 
-    }
-
-
-
+    }  
     myRequest.open("GET","htmlQ2.json",true)
     myRequest.send();
-}
+                       }
 getQuestions();
+
 
 
 function createBullets(num){
     countSpan.innerHTML=num;
-    for( let i = 0; i<num;i++){
-        let theBullet = document.createElement('span');
-        let numberQspan = document.createTextNode(`${i+1}`)
-        theBullet.appendChild(numberQspan);
-        bulletSpanContainer.appendChild(theBullet);
-        if(i === 0){theBullet.className='on';}
+
+    for(let i=0;i<num;i++){
+        let theBullet = document.createElement("span");
+        if(i === 0){
+            theBullet.className= "on";
+        }
+        let numberQSpan =document.createTextNode(`${i+1}`)
+        theBullet.appendChild(numberQSpan);
+        bulletSpanContainer.appendChild(theBullet)
     }
 }
 
-// To add Question and ansewer 
+
+
 function addQuestionsData(obj,countQ){
-if(currentIndex < countQ){
-    let questionTitle = document.createElement('h2');
-    questionTitle.className='wow flipInY';
-    let questionTExt =document.createTextNode(obj["title"]);
-    questionTitle.appendChild(questionTExt);
-    quizArea.appendChild(questionTitle);
+    
+    if(currentIndex < countQ){ 
+let questionTitle = document.createElement('h2')
+questionTitle.className='an wow flipInY'
 
-    for(let i = 1 ; i<=4; i ++){
-let mainDiv = document.createElement('div');
-mainDiv.className='answer  wow fadeInDown';
-let radioInput = document.createElement('input');
-radioInput.name='question';
-radioInput.type='radio';
-radioInput.id=`answer_${i}`;
-radioInput.dataset.answer = obj[`answer_${i}`];
-if(i === 1 ){radioInput.checked=true}
+let questionTExt = document.createTextNode(obj["title"]);
+questionTitle.appendChild(questionTExt)
+quizArea.appendChild(questionTitle)
 
-let theLabel = document.createElement('label');
-let TheLableText =document.createTextNode(obj[`answer_${i}`])
-theLabel.htmlFor =`answer_${i}`;
-theLabel.appendChild(TheLableText);
-mainDiv.appendChild(radioInput)
-mainDiv.appendChild(theLabel);
-answerArea.appendChild(mainDiv)
-    }
+for(var i =1; i<=4; i++){
+    let mainDiv = document.createElement("div");
+    mainDiv.className="answer wow fadeInDown";
+    let radioInput  = document.createElement('input');
+    radioInput.name = "question";
+    radioInput.type = "radio";
+    radioInput.id   = `answer_${i}`;
+    radioInput.dataset.answer = obj[`answer_${i}`];
+if(i===1){ radioInput.checked=true;}
+
+    let TheLabel = document.createElement('label')
+    TheLabel.htmlFor=`answer_${i}`;
+    let TheLableText = document.createTextNode(obj[`answer_${i}`])
+    TheLabel.appendChild(TheLableText)
+    mainDiv.appendChild(radioInput);
+    mainDiv.appendChild(TheLabel);
+    answerArea.appendChild(mainDiv);
+
 }
-}
+                                  }}
 
-function countDownfun(duration,count4){
-if(currentIndex < count4 ){
-    let minutes,secunds;
-let countDownInterval= setInterval(function(){
-    minutes=parseInt(duration / 60);
-    secunds=parseInt(duration % 60);
 
-    minutes =minutes<10 ?`0${minutes}`: minutes;
-    secunds =secunds<10 ?`0${secunds}`: secunds;
-
-    countDown.innerHTML=`${minutes}:${secunds}`
-    if(--duration < 0){
-        clearInterval(countDownInterval);
-        submitButton.click();
-    }
-} ,1000)
-}
-}
 
 function checkAnswer(rAnswer,count2){
-let answers = document.getElementsByName('question');
-let theChoosenAnswer ;
-for(var i =0 ; i < answers.length ; i++){
-if(answers[i].checked){theChoosenAnswer = answers[i].dataset.answer}
-}
-if(theChoosenAnswer === rAnswer){
+   let answers = document.getElementsByName('question')
+   let theChoosenAnswer ;
+   for(var i=0;i<answers.length;i++){
+     if(answers[i].checked){theChoosenAnswer=answers[i].dataset.answer}
+     }
+   if(theChoosenAnswer === rAnswer){
     rightAnswer++;
     console.log(`Good Answer isam the right Answer is ${rAnswer}`)
+                                   }
 }
-}
+
 
 function handelBullet(){
     let bullitSpans3= document.querySelectorAll('.bullets .spans span');
     let ArrayOfSpans = Array.from(bullitSpans3);
-    ArrayOfSpans.forEach((span,index)=>{
+    ArrayOfSpans.forEach((span,index) =>{
         if(currentIndex === index){
-            span.className='on'
-        }
+            span.className='on';}
     })
-
 }
 
 function showResult(count3){
@@ -151,5 +139,22 @@ function showResult(count3){
             theResults =   `<div class="wow wobble"><span class="bad  wow fadeInLeft">Bad</span>,You solve ${rightAnswer} from ${count3} Q bad ${Student} Please agin</div> `
         }
         result.innerHTML=theResults;
+}
+
+}
+function countDownfun(duration,count4){
+if(currentIndex < count4){
+    let minutes,secunds;
+    countDownInterval = setInterval(function(){
+        minutes =parseInt(duration /60);
+        secunds =parseInt(duration %60);
+ minutes =minutes<10?`0${minutes}`: minutes;
+ secunds =secunds<10?`0${secunds}`: secunds;
+        countDown.innerHTML=`${minutes}:${secunds}`
+        if(--duration <0){
+            clearInterval(countDownInterval);
+            submitButton.click();
+        }
+    },1000)
 }
 }
